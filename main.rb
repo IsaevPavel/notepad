b0f496bf15028dcdae6851019065e60d46d1f529
amalgama <isaev.pavelalex@gmail.com>
2022-12-29 14:19:04 +0300

    Notepad v.1, working

---
+++ main.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# Программа «Блокнот», демонстрирующая наследование классов в ruby.
+# Версия 0.2, использующая классы Post, Memo, Link & Task
+
+require_relative 'post'
+require_relative 'memo'
+require_relative 'link'
+require_relative 'task'
+
+puts 'Привет, я твой блокнот!'
+puts
+puts 'Что хотите записать в блокнот?'
+choices = Post.post_types
+choice = -1
+until choice >= 0 && choice < choices.size
+  choices.each_with_index do |type, index|
+    puts "\t#{index}. #{type}"
+  end
+  choice = gets.to_i
+end
+entry = Post.create(choice)
+entry.read_from_console
+entry.save
+puts 'Ваша запись сохранена!'
